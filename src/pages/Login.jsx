f95fa03ed17ac87d1d2f9f9c4d1b909eec056320
gauravPanditr <gauravpandit7856@gmail.com>
2026-08-09 07:59:23 +0530

    changed the url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,7 +10,7 @@ console.log(window.location.origin);
     try {
       await signIn.social({
         provider: "github",
-         callbackURL: `${import.meta.env.VITE_APP_URL}/dashboard`,
+         callbackURL: `https://code-review-client-tau.vercel.app/dashboard`,
       });
     } catch (error) {
       console.error("Login Error:", error);
